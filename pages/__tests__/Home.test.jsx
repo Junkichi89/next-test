6f62f629647ac2980162d7cbb4a7905d2d825eb9
Jumpei Asanuma <jun64pei89@gmail.com>
2022-09-20 14:46:59 +0900

    fix:test

--- pages/__tests__/Home.test.jsx
+++ pages/__tests__/Home.test.jsx
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom'
 import Home from "../index"
 it('Should render title text', () => {
   render(<Home />)
-  expect(screen.getByText('Next')).toBeInTheDocument()
+  expect(screen.getByText('Next.js!')).toBeInTheDocument()
   const heading = screen.getByRole("heading", {
     name: /welcome to next\.js!/i,
   });
